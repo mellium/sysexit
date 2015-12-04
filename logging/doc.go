e25faf889f9cf613fb0c2569789e0641877cad96
Sam Whited <sam@samwhited.com>
2015-12-03 23:19:26 -0600

    Update logging docs

--- logging/doc.go
+++ logging/doc.go
@@ -1,20 +1,3 @@
-// Package logging provides configuration and utility functions for handling
-// logging to files, stderr, and stdout. Honey segregates logs using the
-// standard syslog severity levels as defined in RFC 5424
-// (https://tools.ietf.org/html/rfc5424). Log aggregators for a certain severity
-// level also include all lower levels.
-//
-// The severity levels are:
-//
-//   * Emerg
-//   * Alert
-//   * Crit
-//   * Err
-//   * Warn
-//   * Notice
-//   * Info
-//   * Debug
-//
-// More information on the severity levels (and related constants) can be found
-// in the log/syslog package.
+// Package logging provides a configurable logger for handling logging to files,
+// STDERR, and syslog simultaneously.
 package logging
